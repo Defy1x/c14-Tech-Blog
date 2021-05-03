2142cf4ff5500cf596e46c1cd88013507ec12546
Casandra Mcdonald <casandramcdonald@MacBook-Pro.fios-router.home>
2021-05-02 20:01:40 -0400

    added models and updated server file

--- Develop/models/User.js
+++ Develop/models/User.js
@@ -1,58 +1,54 @@
-const { Model, DataTypes } = require('sequelize');
-const bcrypt = require('bcrypt');
-const sequelize = require('../config/connection');
+const { Model, DataTypes } = require("sequelize");
+const bcrypt = require("bcrypt");
+const sequelize = require("../config/connection.js");
+const { beforeBulkCreate } = require("./Post.js");
 
 class User extends Model {
-  checkPassword(loginPw) {
-    return bcrypt.compareSync(loginPw, this.password);
-  }
-}
+    validatePassword(inputPassword){
+        return bcrypt.compareSync(inputPassword, this.password);
+    }
+};
 
 User.init(
-  {
-    id: {
-      type: DataTypes.INTEGER,
-      allowNull: false,
-      primaryKey: true,
-      autoIncrement: true,
+    {
+        id: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            primaryKey: true,
+            autoIncrement: true
+        },
+        email: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            validate: {
+                isEmail: true
+            }
+        },
+        username: {
+            type: DataTypes.STRING,
+            allowNull: false
+        },
+        password: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            validate: {
+                len: [8]
+            }
+        }
     },
-    name: {
-      type: DataTypes.STRING,
-      allowNull: false,
-    },
-    email: {
-      type: DataTypes.STRING,
-      allowNull: false,
-      unique: true,
-      validate: {
-        isEmail: true,
-      },
-    },
-    password: {
-      type: DataTypes.STRING,
-      allowNull: false,
-      validate: {
-        len: [8],
-      },
-    },
-  },
-  {
-    hooks: {
-      beforeCreate: async (newUserData) => {
-        newUserData.password = await bcrypt.hash(newUserData.password, 10);
-        return newUserData;
-      },
-      beforeUpdate: async (updatedUserData) => {
-        updatedUserData.password = await bcrypt.hash(updatedUserData.password, 10);
-        return updatedUserData;
-      },
-    },
-    sequelize,
-    timestamps: false,
-    freezeTableName: true,
-    underscored: true,
-    modelName: 'user',
-  }
+    {
+        hooks: {
+            async beforeCreate(newUserData){
+                newUserData.password = await bcrypt.hash(newUserData.password, 10);
+                return newUserData;
+            }
+        },
+        sequelize,
+        timestamps: true,
+        freezeTableName: true,
+        underscored: true,
+        modelName: "user"
+    }
 );
 
 module.exports = User;
--- Develop/models/index.js
+++ Develop/models/index.js
@@ -1,13 +1,37 @@
-const User = require('./User');
-const Project = require('./Project');
+const User = require("./User.js");
+const Post = require("./Post.js");
+const Comment = require("./Comment.js");
 
-User.hasMany(Project, {
-  foreignKey: 'user_id',
-  onDelete: 'CASCADE'
+// User - Post/Comment relations
+User.hasMany(Post, {
+    foreignKey: "userId",
+    as: "post_creator"
 });
 
-Project.belongsTo(User, {
-  foreignKey: 'user_id'
+User.hasMany(Comment, {
+    foreignKey: "userId",
+    as: "comment_creator"
 });
 
-module.exports = { User, Project };
+Post.belongsTo(User, {
+    foreignKey: "userId",
+    as: "post_creator"
+});
+
+Comment.belongsTo(User, {
+    foreignKey: "userId",
+    as: "comment_creator"
+});
+
+// Post to comment relations
+Post.hasMany(Comment, {
+    foreignKey: "postId",
+    as: "post_comments"
+});
+
+Comment.belongsTo(Post, {
+    foreignKey: "postId",
+    as: "post_comments"
+});
+
+module.exports = { User, Post, Comment };
