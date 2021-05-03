3ca848df8dd42fb070fedee91329c3f9e7ce5748
Casandra Mcdonald <casandramcdonald@MacBook-Pro.fios-router.home>
2021-05-02 20:11:44 -0400

    added seeds

---
+++ Develop/seeds/userData.js
@@ -0,0 +1,16 @@
+const sequelize = require("../config/connection.js");
+const { User } = require("../models");
+const bcrypt= require("bcrypt");
+
+const userData = [
+    {
+        email: "sqldev@email.com",
+        username: "SQL_Dev",
+        password: "password12345"
+    },
+    {
+        email: "jsdev@email.com",
+        username: "JS_Dev",
+        password: "password12345"
+    }
+];
