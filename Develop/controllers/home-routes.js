e335fcecfe23e720acbd4bfd0dfcf44008451c59
Casandra Mcdonald <casandramcdonald@MacBook-Pro.fios-router.home>
2021-05-04 19:28:42 -0400

    updates to handlebars and routes for posts and comments

--- Develop/controllers/home-routes.js
+++ Develop/controllers/home-routes.js
@@ -1,6 +1,6 @@
 const router = require("express").Router();
 const { User, Post, Comment } = require("../models");
- 
+
 // We grab only the username and comment content from this?
 router.get("/", async (req, res) => {
     try {
@@ -24,6 +24,35 @@ router.get("/", async (req, res) => {
         res.status(500).json(err);
     }
 });
+
+// This is a placeholder, and will need the login page to be rendered.
+router.get("/signin", async (req, res) => {
+    try {
+        res.status(200).render("signin");
+    } catch (err) {
+        res.status(400).json(err);
+    }
+});
+
+// This is a placeholder, and will need the create account page.
+router.get("/signup", async (req, res) => {
+    try {
+        res.status(200).render("signup");
+    } catch (err) {
+        res.status(500).json(err);
+    }
+})
+
+// This is a placeholder, and will need the logout page to be rendered.
+router.get("/signout", async (req, res) => {
+    try {
+        res.status(200).render("signout");
+    } catch (err) {
+        res.status(500).json(err);
+    }
+})
+
+module.exports = router;
 
 // This is a placeholder, and will need the login page to be rendered.
 router.get("/signin", async (req, res) => {
---
+++ Develop/public/js/comment-admin.js
@@ -0,0 +1,74 @@
+// Contains all the related functionality about creating, updating, and deleting comments.
+// These follow the same structure as the post-admin fetch calls since the backend API functions pretty similarly.
+// NOTE: These all need to be linked to the DOM and Handlebars templates before they are functional.
+
+// The properties we need to create a comment are the userId, the postId, and the new comment's text, so these need to be grabbed from the DOM accordingly.
+const createComment = async (event) => {
+    event.stopPropagation();
+
+    const userId = document.querySelector(".user-profile").getAttribute("data-currentuser");
+    console.log(userId);
+    const postId = event.target.getAttribute("data-postid");
+    console.log(postId);
+    const commentContent = document.querySelector(".input-comment").value;
+
+    const response = await fetch(`/api/comments/${userId}/${postId}`, {
+        method: "POST",
+        body: JSON.stringify({ userId, postId, commentContent }),
+        headers: { "Content-Type": "application/json" }
+    });
+    if (response.ok) {
+        document.location.reload();
+    } else {
+        alert("Failed to create comment.");
+    }
+};
+
+// Couple of explanations for this one:
+// 1) We want to store the currently logged in user's ID somewhere in the DOM for future reference.
+// 2) Once we have that, we can use that to compare against the IDs assigned to comments, blog posts, whatever.
+// 3) I'm pretty sure there's better ways to to handle this but I'm tired
+const updateComment = async (event) => {
+    event.stopPropagation();
+
+    const currentUserId = document.querySelector(".user-profile").getAttribute("data-currentuser");
+    const commentUserId = event.target.getAttribute("data-userid");
+    const commentId = event.target.getAttribute("data-commentid");
+    const commentContent = document.querySelector(`.comment-content-${commentId}`).textContent;
+    if (currentUserId !== commentUserId){
+        return alert("You cannot edit another user's comments!");
+    }
+    const response = await fetch(`/api/comments/${commentId}`, {
+        method: "PUT",
+        body: JSON.stringify({ commentContent }),
+        headers: { "Content-Type": "application/json" }
+    });
+    if (response.ok) {
+        document.location.reload();
+    } else {
+        alert("Failed to update comment.");
+    }
+};
+
+const deleteComment = async (event) => {
+    event.stopPropagation();
+
+    const commentId = event.target.getAttribute("data-commentid");
+    const response = await fetch(`/api/commentss/${commentId}`, {
+        method: "DELETE",
+        headers: { "Content-Type": "application/json" }
+    });
+    if (response.ok) {
+        document.location.reload();
+    } else {
+        alert("Failed to delete comment.");
+    }
+};
+
+document.querySelector(".new-comment").addEventListener("click", createComment);
+document.querySelectorAll(".update-comment").forEach((comment) => {
+    comment.addEventListener("click", updateComment);
+});
+document.querySelectorAll(".delete-comment").forEach((comment) => {
+    comment.addEventListener("click", deleteComment);
+});
