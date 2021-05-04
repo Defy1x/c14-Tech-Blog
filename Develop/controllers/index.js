f2d624b247baeb9abd87eefb80e817d622d02495
Casandra Mcdonald <casandramcdonald@MacBook-Pro.fios-router.home>
2021-05-03 20:43:36 -0400

    middlewear working and redirecting user to login

--- Develop/controllers/index.js
+++ Develop/controllers/index.js
@@ -4,10 +4,11 @@ const apiRoutes = require("./api");
 const homeRoutes = require("./home-routes");
 const postRoutes = require("./singleblogpost-routes");
 const dashboardRoutes = require("./dashboard-routes");
+const checkAuthorization = require('../utils/authorization');
 
 router.use("/", homeRoutes);
-router.use("/api", apiRoutes);
-router.use("/post", postRoutes);
-router.use("/dashboard", dashboardRoutes);
+router.use("/api", checkAuthorization, apiRoutes);
+router.use("/post", checkAuthorization, postRoutes);
+router.use("/dashboard", checkAuthorization, dashboardRoutes);
 
 module.exports = router;
