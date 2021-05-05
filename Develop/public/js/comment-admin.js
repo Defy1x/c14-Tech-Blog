496832c0af01626cc12df2fab4eaaa069ad8870f
Casandra Mcdonald <casandramcdonald@MacBook-Pro.fios-router.home>
2021-05-04 23:44:55 -0400

    everything works but looks like shit

--- Develop/public/js/comment-admin.js
+++ Develop/public/js/comment-admin.js
@@ -1,8 +1,3 @@
-// Contains all the related functionality about creating, updating, and deleting comments.
-// These follow the same structure as the post-admin fetch calls since the backend API functions pretty similarly.
-// NOTE: These all need to be linked to the DOM and Handlebars templates before they are functional.
-
-// The properties we need to create a comment are the userId, the postId, and the new comment's text, so these need to be grabbed from the DOM accordingly.
 const createComment = async (event) => {
     event.stopPropagation();
 
@@ -11,6 +6,7 @@ const createComment = async (event) => {
     const postId = event.target.getAttribute("data-postid");
     console.log(postId);
     const commentContent = document.querySelector(".input-comment").value;
+    console.log(commentContent);
 
     const response = await fetch(`/api/comments/${userId}/${postId}`, {
         method: "POST",
@@ -32,9 +28,11 @@ const updateComment = async (event) => {
     event.stopPropagation();
 
     const currentUserId = document.querySelector(".user-profile").getAttribute("data-currentuser");
+    console.log(currentUserId);
     const commentUserId = event.target.getAttribute("data-userid");
+    console.log(commentUserId);
     const commentId = event.target.getAttribute("data-commentid");
-    const commentContent = document.querySelector(`.comment-content-${commentId}`).textContent;
+    const commentContent = document.querySelector(`.input-content-${commentId}`).textContent;
     if (currentUserId !== commentUserId){
         return alert("You cannot edit another user's comments!");
     }
@@ -54,7 +52,8 @@ const deleteComment = async (event) => {
     event.stopPropagation();
 
     const commentId = event.target.getAttribute("data-commentid");
-    const response = await fetch(`/api/commentss/${commentId}`, {
+    console.log(commentId);
+    const response = await fetch(`/api/comments/${commentId}`, {
         method: "DELETE",
         headers: { "Content-Type": "application/json" }
     });
